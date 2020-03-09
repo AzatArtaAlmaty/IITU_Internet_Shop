c10d546e433114936fc812b2bbe0c8afcdf9dd08
Erma <malgazhdar.ermahan@gmail.com>
2020-03-09 23:25:55 +0600

    Merge branch 'master' of C:\Users\Sulpak\Documents\If you delete need files\Design\Projects\Outfit\IITU_Internet_Shop with conflicts.

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -23,8 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-//                    .antMatchers("/", "/css/**").permitAll()
-                    .anyRequest().permitAll()
+                    .antMatchers("/", "/css/**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
                     .httpBasic()
                 .and()
